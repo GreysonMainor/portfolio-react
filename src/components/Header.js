0ccca548e83d2f44c53769147eae01e7d445ec69
Greyson Mainor <gbmainor98@outlook.com>
2021-05-10 11:12:36 -0400

    added particles and styling to my header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react'
-//import Typed from "react-typed"
+import Typed from "react-typed"
 const Header = () => {
     return (
      <div className="header-wrapper">
          <div className="main-info">
              <h1>web development and websites promotions</h1>
-             {/* <Typed 
+             <Typed 
              className="typed-text"
              strings={["Web Design", "Web Development"]}
-             /> */}
+             typeSpeed={40}
+             backSpeed={60}
+             loop
+             /> 
+             <a href="#" className="btn-main-offer">Contact Me</a>
 
          </div>
      </div>
