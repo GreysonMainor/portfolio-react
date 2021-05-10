c412ac0acf4e0856205fce405943cc6307001058
Greyson Mainor <gbmainor98@outlook.com>
2021-05-09 21:32:57 -0400

    began work on my header.

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+//import Typed from "react-typed"
+const Header = () => {
+    return (
+     <div className="header-wrapper">
+         <div className="main-info">
+             <h1>web development and websites promotions</h1>
+             {/* <Typed 
+             className="typed-text"
+             strings={["Web Design", "Web Development"]}
+             /> */}
+
+         </div>
+     </div>
+    )
+}
+
+export default Header
