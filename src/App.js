849ed94ee1d6504e89f6920a7e8014deec10e3dc
Greyson Mainor <gbmainor98@outlook.com>
2021-05-11 13:37:20 -0400

    began working on contact page.

--- src/App.js
+++ src/App.js
@@ -5,39 +5,49 @@ import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import AboutMe from "./components/AboutMe"
 import Portfolio from "./components/Portfolio"
+import Contacts from "./components/Contacts";
 
 
 
 
 function App() {
-  return (
-    <>
-      <Particles
-        className="particles-canvas"
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 900
-              }
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 6,
-                color: "#f9ab00"
-              }
+    return ( <
+        >
+        <
+        Particles className = "particles-canvas"
+        params = {
+            {
+                particles: {
+                    number: {
+                        value: 30,
+                        density: {
+                            enable: true,
+                            value_area: 900
+                        }
+                    },
+                    shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 6,
+                            color: "#f9ab00"
+                        }
+                    }
+                }
             }
-          }
-        }} />
-      <Navbar />
-      <Header />
-      <AboutMe />
-      <Portfolio />
-    </>
-  );
+        }
+        /> <
+        Navbar / >
+        <
+        Header / >
+        <
+        AboutMe / >
+        <
+        Portfolio / >
+        <
+        Contacts / >
+        <
+        />
+    );
 }
 
 export default App;
---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contacts = () => {
+    return ( <
+        div className = "contacts" >
+        <
+        h1 > contact me < /h1> <
+        p > please fill out forms and i will contact you < /p> <
+        /div>
+    )
+}
+
+export default Contacts
