aa095374c012bfe0fd3b19e6ff5c0d64c1242ceb
Greyson Mainor <gbmainor98@outlook.com>
2021-05-11 16:58:32 -0400

    added functionality to my contact page.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,57 +1,94 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import emailjs from "emailjs-com"
+import {useForm} from "react-hook-form";
 
 const Contacts = () => {
+    const [successMessage, setSuccessMessage] = useState("");
+    const{ register, handleSubmit, errors } = useForm();
+
+const serviceID = "service_ID";
+const templateID = "template_ID";
+const userID = "user_YoBfJGvjkfqS01qefLswu";
+
+const onSubmit = (data, r) => {
+    sendEmail(
+        serviceID,
+         templateID,
+         {
+             name: data.name,
+             phone: data.phone,
+             email: data.email,
+             subject: data.subject,
+             description: data.description
+         },
+         userID
+         )
+         r.target.reset();
+}
+
+    function sendEmail(serviceID, templateID, variables, userID) {
+        emailjs.send(serviceID, templateID, variables, userID)
+          .then(() => {
+              setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.");
+          }).catch(err => console.error(`something went wrong ${err}`));
+      }
+    
+
     return (
         <div className="contacts">
             <div className="text-center">
             <h1>contact me</h1>
-            <p>please fill out forms and i will contact you</p>
+            <p>Please fill out the form below and I will contact you as soon as possible.</p>
+            <span className="success-message"></span>
             </div>
             <div className="container">
+                <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="row">
                     <div className="col-md-6 col-xs-12">
                         <input
-                        id="name"
                         type="text"
                         className="form-control"
                         placeholder="Name"
                         name="name"
                          />
+                         <div className="line"></div>
+                         <span className="error-message">
+                         </span>
                            <input
-                           id="phone"
                            type="text"
                         className="form-control"
                         placeholder="Phone Number"
-                        phone="phone"
+                        name="phone"
                          />
+                         <div className="line"></div>
                           <input
-                          id="email"
                           type="email"
                         className="form-control"
                         placeholder="Email"
-                        email="email"
+                        name="email"
                          />
+                         <div className="line"></div>
                           <input
-                          id="subject"
                           type="text"
                         className="form-control"
                         placeholder="Subject"
-                        subject="subject"
+                        name="subject"
                          />
                     </div>
                     <div className="col-md-6 col-xs-12">
                     <textarea
-                    id="description"
                     type="text"
                         className="form-control"
-                        placeholder="Phone Number"
-                        phone="phone"
+                        placeholder="Description"
+                        name="description"
                          ></textarea>
+                         <div className="line"></div>
                          <button className="btn-main-offer contact-btn" type="submit">Contact Me</button>
 
                     </div>
                 </div>
+                </form>
             </div>
         </div>
     )
